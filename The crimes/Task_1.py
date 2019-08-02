a32cc6ac147762fddf303e369745d4702d63b403
Gendalf <sirartic67@gmail.com>
2019-08-02 15:58:20 +0300

    Done

--- The crimes/Task_1.py
+++ The crimes/Task_1.py
@@ -10,4 +10,26 @@
 10 баллов
 10 минут
 """
-
+from json import loads,dumps
+dct = {}
+with open("Crimes.csv", "r") as file:
+    keys = file.readline().split(",")
+    print(keys)
+    for line in file:
+        lst = line.split(",")
+        print(lst)
+        if lst[5] not in dct:
+            dct[lst[5]] = []
+        dct[lst[5]].append({
+            keys[0]: lst[0],
+            keys[1]: lst[1],
+            keys[2]: lst[2],
+            keys[3]: lst[3],
+            keys[4]: lst[4],
+            keys[6]: lst[6],
+            keys[7]: lst[7],
+            keys[8]: lst[8]
+        })
+with open("Info.txt", "w") as file:
+    file.write(dumps(dct))
+print(dumps(dct, indent=4))
