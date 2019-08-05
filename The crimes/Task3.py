c7960a0102e3a033b7a21c4f25e16df7ea1b413e
Gendalf <sirartic67@gmail.com>
2019-08-05 14:19:55 +0300

    created Task3 and types.txt

---
+++ The crimes/Task3.py
@@ -0,0 +1,11 @@
+from json import loads, dumps
+with open("Info.txt", "r") as file:
+    dct = loads(file.read())
+with open("types.txt", "r") as file:
+    types_list = file.read().split(",")
+for name in dct:
+    for crime in dct[name]:
+        for i in range(len(types_list)):
+            if crime["Type"] == types_list[i]:
+                if name != "-":
+                    print(name)
