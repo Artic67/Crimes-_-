e346730dea6ad64a279a0b9e13aa2b97e4d290e7
Gendalf <sirartic67@gmail.com>
2019-08-05 14:25:22 +0300

    Second update

--- The crimes/Task3.py
+++ The crimes/Task3.py
@@ -3,9 +3,11 @@ with open("Info.txt", "r") as file:
     dct = loads(file.read())
 with open("types.txt", "r") as file:
     types_list = file.read().split(",")
+names = set()
 for name in dct:
     for crime in dct[name]:
         for i in range(len(types_list)):
             if crime["Type"] == types_list[i]:
-                if name != "-":
+                if name != "-" and name not in names:
                     print(types_list[i], ":", name)
+                    names.add(name)
