342612800d3f79a41ce4f7aac4369d26a70d9a58
Gendalf <sirartic67@gmail.com>
2019-08-05 14:54:26 +0300

    Task4

---
+++ The crimes/Task4.py
@@ -0,0 +1,16 @@
+from json import loads, dumps
+with open("Info.txt", "r") as file:
+    dct = loads(file.read())
+with open("Ans.txt", "w") as file:
+    pass
+name1 = input("Name: ")
+for name in dct:
+    for crime in dct[name]:
+        lst = []
+        if name1 == name:
+            for key in crime:
+                lst.append(crime[key])
+            with open("Ans.txt", "a") as file:
+                for el in range(len(lst)):
+                    lst[el] = str(lst[el])
+                file.write(name1 + ";" + ";".join(lst) + "\n")
